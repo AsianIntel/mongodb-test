4fc7c78c6bc5e2d0449931cab273850a90cfc4d8
AsianIntel <gautam.abhyankar@gmail.com>
2021-06-27 19:36:38 +0530

    Initial commit

---
+++ src/client.rs
@@ -0,0 +1,126 @@
+use hyper::{
+    body::{self, Buf},
+    client::HttpConnector,
+    Body,
+    Client as HyperClient,
+    Error as HyperError,
+    Method,
+    Request,
+    Response,
+};
+use serde::Deserialize;
+use serde_json::Error as SerdeError;
+
+#[derive(Clone, Debug, Default)]
+pub(crate) struct HttpClient {
+    inner: HyperClient<HttpConnector>,
+}
+
+#[derive(Debug)]
+pub(crate) enum HttpError {
+    BuildingRequest,
+    Request(HyperError),
+    InvalidUTF8,
+    Parsing(SerdeError),
+}
+
+impl HttpClient {
+    /// Executes an HTTP GET request and deserializes the JSON response.
+    pub(crate) async fn get_and_deserialize_json<'a, T>(
+        &self,
+        uri: &str,
+        headers: impl IntoIterator<Item = &'a (&'a str, &'a str)>,
+    ) -> Result<T, HttpError>
+    where
+        T: for<'de> Deserialize<'de>,
+    {
+        let res = self.request(Method::GET, uri, headers).await?;
+
+        let mut buf = body::aggregate(res.into_body()).await?;
+        let mut bytes = vec![0; buf.remaining()];
+        buf.copy_to_slice(&mut bytes);
+
+        let result = serde_json::from_slice(&bytes)?;
+        Ok(result)
+    }
+
+    /// Executes an HTTP GET request and returns the response body as a string.
+    pub(crate) async fn get_and_read_string<'a>(
+        &self,
+        uri: &str,
+        headers: impl IntoIterator<Item = &'a (&'a str, &'a str)>,
+    ) -> Result<String, HttpError> {
+        self.request_and_read_string(Method::GET, uri, headers)
+            .await
+    }
+
+    /// Executes an HTTP PUT request and returns the response body as a string.
+    pub(crate) async fn put_and_read_string<'a>(
+        &self,
+        uri: &str,
+        headers: impl IntoIterator<Item = &'a (&'a str, &'a str)>,
+    ) -> Result<String, HttpError> {
+        self.request_and_read_string(Method::PUT, uri, headers)
+            .await
+    }
+
+    /// Executes an HTTP request and returns the response body as a string.
+    pub(crate) async fn request_and_read_string<'a>(
+        &self,
+        method: Method,
+        uri: &str,
+        headers: impl IntoIterator<Item = &'a (&'a str, &'a str)>,
+    ) -> Result<String, HttpError> {
+        let res = self.request(method, uri, headers).await?;
+
+        let mut buf = body::aggregate(res.into_body()).await?;
+        let mut bytes = vec![0; buf.remaining()];
+        buf.copy_to_slice(&mut bytes);
+
+        let text = String::from_utf8(bytes)?;
+        Ok(text)
+    }
+
+    /// Executes an HTTP equest and returns the response.
+    pub(crate) async fn request<'a>(
+        &self,
+        method: Method,
+        uri: &str,
+        headers: impl IntoIterator<Item = &'a (&'a str, &'a str)>,
+    ) -> Result<Response<Body>, HttpError> {
+        let mut request = Request::builder().uri(uri).method(method);
+
+        for header in headers {
+            request = request.header(header.0, header.1);
+        }
+
+        let request = request.body(Body::empty())?;
+        let response = self.inner.request(request).await?;
+
+        Ok(response)
+    }
+}
+
+impl From<hyper::http::Error> for HttpError {
+    fn from(_err: hyper::http::Error) -> Self {
+        Self::BuildingRequest
+    }
+}
+
+impl From<HyperError> for HttpError {
+    fn from(err: HyperError) -> Self {
+        Self::Request(err)
+    }
+}
+
+impl From<SerdeError> for HttpError {
+    fn from(err: SerdeError) -> Self {
+        Self::Parsing(err)
+    }
+}
+
+impl From<std::string::FromUtf8Error> for HttpError {
+    fn from(_err: std::string::FromUtf8Error) -> Self {
+        Self::InvalidUTF8
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,75 @@
+#![allow(dead_code)]
+
+mod client;
+
+use std::error::Error;
+use client::{HttpClient, HttpError};
+use serde::Deserialize;
+
+const AWS_ECS_IP: &str = "169.254.170.2";
+const AWS_EC2_IP: &str = "169.254.169.254";
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let client = HttpClient::default();
+    let creds = AwsCredential::get_from_ec2(&client).await.unwrap();
+    println!("{:?}", creds);
+
+    Ok(())
+}
+
+#[derive(Debug, Deserialize)]
+struct AwsCredential {
+    #[serde(rename = "AccessKeyId")]
+    access_key: String,
+
+    #[serde(rename = "SecretAccessKey")]
+    secret_key: String,
+
+    #[serde(rename = "Token")]
+    session_token: Option<String>,
+}
+
+impl AwsCredential {
+
+    /// Obtains credentials from the ECS endpoint.
+    async fn get_from_ecs(relative_uri: String, http_client: &HttpClient) -> Result<Self, HttpError> {
+        // Use the local IP address that AWS uses for ECS agents.
+        let uri = format!("http://{}/{}", AWS_ECS_IP, relative_uri);
+
+        http_client
+            .get_and_deserialize_json(&uri, None)
+            .await
+    }
+
+    /// Obtains temporary credentials for an EC2 instance to use for authentication.
+    async fn get_from_ec2(http_client: &HttpClient) -> Result<Self, HttpError> {
+        let temporary_token = http_client
+            .put_and_read_string(
+                &format!("http://{}/latest/api/token", AWS_EC2_IP),
+                &[("X-aws-ec2-metadata-token-ttl-seconds", "30")],
+            )
+            .await?;
+
+        let role_name_uri = format!(
+            "http://{}/latest/meta-data/iam/security-credentials/",
+            AWS_EC2_IP
+        );
+
+        let role_name = http_client
+            .get_and_read_string(
+                &role_name_uri,
+                &[("X-aws-ec2-metadata-token", &temporary_token[..])],
+            )
+            .await?;
+
+        let credential_uri = format!("{}/{}", role_name_uri, role_name);
+
+        http_client
+            .get_and_deserialize_json(
+                &credential_uri,
+                &[("X-aws-ec2-metadata-token", &temporary_token[..])],
+            )
+            .await
+    }
+}
